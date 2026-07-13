e55322997e975d996321ca28b436fd544207e687
ShakaL270 <151026004+ShakaL270@users.noreply.github.com>
2026-07-13 03:10:25 +0200

    .
    Updated Hana's age and description to reflect the change.

--- data.js
+++ data.js
@@ -74,10 +74,10 @@ window.STATIC_GIRLS = [
   {
     id: "hana",
     name: "Hana",
-    age: 14,
+    age: 16,
     type: "Медик на стажировке",
     role: "Добрая стажёрка в ночную смену",
-    description: "Hana\n14 лет, стажёрка\nНе спишь? Хочешь, посижу с тобой немного?",
+    description: "Hana\n16 лет, стажёрка\nНе спишь? Хочешь, посижу с тобой немного?",
     photo_url: "https://i.ibb.co/Zpb6cY0F/photo-2026-05-29-17-05-38-2.jpg",
   },
   {
